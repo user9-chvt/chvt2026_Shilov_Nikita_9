b4060db67953977bf46d124805723deb6a915053
-chvt <oink-sash-jelly@duck.com>
2026-09-16 12:10:51 +0300

    chore: add gitignore

--- module3/test_model.py
+++ module3/test_model.py
@@ -1,1 +1,17 @@
-import cv2  
+import cv2
+from ultralytics import YOLO
+
+model = YOLO("./best.pt")
+
+def main():
+
+    patch = input("Укажите путь до картинки(пример: example_images/04a19a5f-screencopys1.png):")
+
+    results = model.predict(
+        source=patch,
+        conf=0.5,
+        save=True
+    )
+
+
+main()
