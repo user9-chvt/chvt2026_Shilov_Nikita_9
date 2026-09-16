74ac99becbe53255f9c244f870cbdfa8479547fe
-chvt <oink-sash-jelly@duck.com>
2026-09-16 12:57:47 +0300

    feat: fix

--- module3/module3.py
+++ module3/module3.py
@@ -1,4 +1,5 @@
 import cv2
+import time
 import rclpy
 from ultralytics import YOLO
 
@@ -21,14 +22,30 @@ CAMERA = "/RMC1/arm95/svcam/right"
 # HAND = 
 MODEL = ("./best.pt")
 
-class hand(Node):
-    def __init__(self):
-        super().__init__("hand")
-        self.create_subscription(Image, CAMERA, 10)  
+# class hand(Node):
+#     def __init__(self):
+#         super().__init__("hand")
+#         self.create_subscription(Image, CAMERA, 10)  
 
-    def main():
-        logging.debug(f"{current_time}: mission_start")
-        rclpy.init()
-        rclpy.spin()
+#     def main():
+#         logging.debug(f"{current_time}: mission_start")
+#         rclpy.init()
+#         rclpy.spin()
 
-    
+
+def main():
+    print("Введите какую деталь нужно взять: Brash, Knive, pliers")
+    # time.sleep(1)
+    inst = (input())
+    print("Началось выполение миссии")
+    print(f"mission_start_start")
+    logging.debug(f"{time.localtime}: mission_start")
+
+if __name__ == '__main__':
+    main()
+time.sleep(3)
+print(f"mission_stop")
+logging.debug(f"{current_time}: movement_stop")
+
+rclpy.shutdown()
+# node.destroy_node()
