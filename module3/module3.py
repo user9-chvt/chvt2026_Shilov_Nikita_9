081ad0570d8e4c69b851a104fe4bb216c4209add
-chvt <oink-sash-jelly@duck.com>
2026-09-16 12:52:51 +0300

    docs: обновлено основное ридми и ридми в модуле

--- module3/module3.py
+++ module3/module3.py
@@ -1,24 +1,34 @@
 import cv2
 import rclpy
-from rclpy import Node
+from ultralytics import YOLO
+
+from rclpy.node import Node
 
 from pathlib import Path
-
 from threading import Thread
+
+from rclpy import qos_overriding_options
+
+from sensor_msgs.msg import Image
 
 #for logs
 import logging
 from datetime import datetime
+current_time = datetime.now()
 
-HERE = Path.resolve().parent("best.pt")
+# HERE = Path.resolve().parent("best.pt")
 CAMERA = "/RMC1/arm95/svcam/right"
 # HAND = 
+MODEL = ("./best.pt")
 
 class hand(Node):
     def __init__(self):
         super().__init__("hand")
-        
+        self.create_subscription(Image, CAMERA, 10)  
+
     def main():
+        logging.debug(f"{current_time}: mission_start")
         rclpy.init()
+        rclpy.spin()
 
     
